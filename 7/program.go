0a69d63629d6bbe4c9587204153fb03324eb3c4b
IvanovAA <ivanovaa@ugsk.ru>
2015-10-06 17:27:38 +0500

    Solved http://acmu.ru/?main=task&id_task=7

---
+++ 7/program.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"io/ioutil"
+	"strings"
+)
+
+func main() {
+	content, _ := ioutil.ReadFile("input.txt")
+	lines := strings.Split(string(content), "\n")
+	numbers := strings.Split(strings.Trim(lines[0], "\t \r"), " ")
+	var output = max(max(numbers[0], numbers[1]), numbers[2])
+	ioutil.WriteFile("output.txt", []byte(output), 0)
+}
+
+func max(a, b string) string {
+	la := len(a)
+	lb := len(b)
+
+	if la > lb {
+		return a
+	} else {
+		return b
+	}
+
+	if la == lb {
+		for i := 0; i < la; i++ {
+			if a[i] > b[i] {
+				return a
+			} else {
+				return b
+			}
+		}
+	}
+	return a
+}
