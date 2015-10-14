c599a668a2d08a5ba9259a342dcf5b0d1240f2a9
solo12zw74 <solo12zw74@gmail.com>
2015-10-14 23:43:19 +0500

    Solved http://acmu.ru/?main=task&id_task=11

--- 11/program.go
+++ 11/program.go
@@ -2,21 +2,31 @@ package main
 
 import (
 	"fmt"
+	"math/big"
 	"os"
 )
 
 func main() {
 	input, _ := os.Open("input.txt")
 
-	result := "NO\n"
-
-	var a, b int
-	_, _ = fmt.Fscanf(input, "%d %d\n", &a, &b)
+	var k, n int
+	_, _ = fmt.Fscanf(input, "%d %d\n", &k, &n)
 
 	input.Close()
 
-	
+	arr := make([]*big.Int, 301)
+
+	arr[0] = big.NewInt(1)
+	for i := 1; i <= n; i++ {
+		arr[i] = big.NewInt(0)
+		for j := 1; j <= k; j++ {
+			if i-j >= 0 {
+				arr[i].Add(arr[i], arr[i-j])
+			}
+		}
+	}
+
 	output, _ := os.Create("output.txt")
-	output.WriteString(result)
+	output.WriteString(fmt.Sprintf("%d\n", arr[n]))
 	output.Close()
 }
